5be1d0a5bffbc5b2e274cce02c9caae2a2336de6
Riley Andrews <riandrews@google.com>
2015-09-01 12:42:07 -0700

    android: binder: Use wake up hint for synchronous transactions.
    Use wake_up_interruptible_sync() to hint to the scheduler binder
    transactions are synchronous wakeups. Disable premption while waking
    to avoid ping-ponging on the binder lock.
    
    Change-Id: Ic406a232d0873662f80148e37acefe5243d912a0

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2266,7 +2266,9 @@ static void binder_transaction(struct binder_proc *proc,
 	}
 	if (target_wait) {
 		if (reply || !(t->flags & TF_ONE_WAY)) {
+			preempt_disable();
 			wake_up_interruptible_sync(target_wait);
+			sched_preempt_enable_no_resched();
 		}
 		else {
 			wake_up_interruptible(target_wait);
